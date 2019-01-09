880a2669b6383f29032295d18dfed373054693af
Unknown <18335929360@163.com>
2019-01-09 09:51:08 +0800

    Learn Odin plugins for Unity

---
+++ Assets/Scripts/OdinStudy/Scripts/OdinTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+
+public class OdinTest : SerializedMonoBehaviour
+{
+    [Title("数据类型")]
+    [FoldoutGroup("基本数据")]
+    //基本数据类型
+    public bool boolValue;
+  
+    [ShowIf("boolValue")]
+    [FoldoutGroup("基本数据")]
+    public string str;
+
+    [MinValue(0)]
+    [FoldoutGroup("基本数据")]
+    public int minValueLimit = 1;
+
+    [MaxValue(100)]
+    [FoldoutGroup("基本数据")]
+    public int maxValueLimit = 99;
+
+    [Wrap(0, 100)]
+    [FoldoutGroup("基本数据")]
+    public int valueLimit = 50;
+
+    [MinMaxSlider(0, 100)]
+    public Vector2 v;
+
+    [ProgressBar(0, 100, ColorMember = "ChangeColor")]
+    public float hpBar;
+
+    public Color ChangeColor(float value)
+    {
+        return Color.Lerp(Color.red, Color.green, MathUtilities.LinearStep(0, 100, value));
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    [EnumToggleButtons]
+    public Direction direction;
+
+    [Title("数据结构")]
+    [ListDrawerSettings(NumberOfItemsPerPage = 5)]
+    public List<int> list;
+
+    public Dictionary<int, string> map;
+
+    //PopUp
+    [ListDrawerSettings(NumberOfItemsPerPage = 5)]
+    public List<string> stringList;
+
+    public string selectString;
+}
---
+++ Assets/Scripts/OdinStudy/Scripts/OdinTestEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+using System.Linq;
+
+[CustomEditor(typeof(OdinTest))]
+public class OdinTestEditor : OdinEditor
+{
+   private OdinTest _odinTest { get { return target as OdinTest; } }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        int selectIndex = 0;
+
+        if(_odinTest.stringList.Count > 1)
+        {
+            for (int i = 0; i < _odinTest.stringList.Count; i++)
+            {
+                if(_odinTest.selectString == _odinTest.stringList[i])
+                {
+                    selectIndex = i;
+                    break;
+                }
+            }
+        }
+        //EditorGUILayout.Popup("SelectString", selectIndex, _odinTest.stringList.ToArray());
+        //_odinTest.selectString = _odinTest.stringList[selectIndex]; 
+
+        //绘制搜索框
+        if(GUILayout.Button(_odinTest.selectString,"MiniPopup"))
+        {
+            //false表示不能复数选择
+            GenericSelector<string> selector = new GenericSelector<string>("SelectString", false, _odinTest.stringList);
+            selector.SetSelection(_odinTest.stringList[selectIndex]);
+            selector.SelectionTree.DefaultMenuStyle.Height = 20;
+
+            selector.SelectionConfirmed += stringlist =>
+           {
+               _odinTest.selectString = stringlist.FirstOrDefault();
+           };
+            selector.SelectionTree.Config.DrawSearchToolbar = true;
+
+            var window = selector.ShowInPopup();
+            window.OnClose += selector.SelectionTree.Selection.ConfirmSelection;
+        }
+    }
+}
